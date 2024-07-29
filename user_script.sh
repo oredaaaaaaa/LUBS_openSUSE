b2519d5f05a4adb42a868fbbac89876dbaaf3630
Yamanote_F2L有限公司 <126127291+oredaaaaaaa@users.noreply.github.com>
2024-07-29 11:27:45 +0900

    Fcitx5の自動起動設定を追加

--- user_script.sh
+++ user_script.sh
@@ -2,4 +2,9 @@
 # ユーザーが自由に編集できるスクリプト
 echo "ユーザーのカスタムスクリプトが実行されています。"
 
-
+# Fcitx5の自動起動設定
+echo "export XMODIFIERS=@im=fcitx5" >> /etc/profile.d/fcitx5.sh
+echo "export GTK_IM_MODULE=fcitx5" >> /etc/profile.d/fcitx5.sh
+echo "export QT_IM_MODULE=fcitx5" >> /etc/profile.d/fcitx5.sh
+echo "export SDL_IM_MODULE=fcitx5" >> /etc/profile.d/fcitx5.sh
+echo "export GLFW_IM_MODULE=fcitx5" >> /etc/profile.d/fcitx5.sh
